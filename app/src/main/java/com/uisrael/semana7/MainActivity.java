a79f2def2ad548e9e860ccd62ee29e3dbcf16ab8
jlmaigua <cheverepepin@gmail.com>
2019-11-28 18:43:25 -0500

    Tercera Version

--- app/src/main/java/com/uisrael/semana7/MainActivity.java
+++ app/src/main/java/com/uisrael/semana7/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //cambio por consola
+        //cambio de modo grafico
+
+        //agregar otro comentario por consola
     }
 }
